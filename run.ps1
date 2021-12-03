e2b393c0e6676455421462866dd23c1a4cd3f342
Paulo Ferreira <gammalambda333@gmail.com>
2021-12-03 20:34:47 +0000

    Added utility compile & run script

---
+++ run.ps1
@@ -0,0 +1,23 @@
+$target = $args[0]
+if ($null -eq $target) {
+    $cmdName = $MyInvocation.MyCommand
+    Write-Host "usage: $cmdName <dayNN>"
+    return
+}
+
+$compilationDir = New-Item -ItemType Directory -Force -Path "obj"
+$binDir = New-Item -ItemType Directory -Force -Path "bin"
+$srcDir = "src"
+$utilsModule = "utils"
+
+Push-Location $compilationDir
+sh mmc -i "../$srcDir/$utilsModule.m"
+$compileResult = $?
+
+sh mmc "../$srcDir/$utilsModule.m" "../$srcDir/$target.m" -o "$binDir\$target"
+$compileResult = $compileResult -and $?
+Pop-Location
+
+if ($compileResult) {
+    & "$binDir\$target.exe"
+}
